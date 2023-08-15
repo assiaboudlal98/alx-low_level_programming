586a3b4b2c84e7fde72b45dd7a860397e18451ee
assiaboudlal98 <boudlala22@gmail.com>
2023-08-14 18:47:44 -0700

    My first commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -29,7 +29,7 @@ int main(void)
 						putchar(digit2 + 48);
 						putchar(digit3 + 48);
 
-						if (digit1 + digit2 = digit3 != 24)
+						if (digit1 + digit2 + digit3 != 24)
 						{
 							putchar(',');
 							putchar(' ');
