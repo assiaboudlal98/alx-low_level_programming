04aefcef2be79e2f149f37e8705360fd100bdabd
assiaboudlal98 <boudlala22@gmail.com>
2023-08-14 17:23:44 -0700

    My first commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,12 +6,14 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int digit = 48; /*48; decimal rep of 0*/
 
 	while (digit <= 102 /*102; decimal rep of f*/
 	{
+	
 		putchar(digit);
 
 		/* after 9 we jump till 96; `*/
