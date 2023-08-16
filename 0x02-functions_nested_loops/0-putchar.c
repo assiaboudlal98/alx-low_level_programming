7cfa120397694d03114c5c6bf748b888be5370f7
assiaboudlal98 <boudlala22@gmail.com>
2023-08-16 14:17:54 -0700

    My first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,16 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int mai(void)
 {
-	char str[] = "_putchar";
-	int ch;
-
-	for (char = 0; ch < 8; ch++)
-		_putchar(str[ch]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
