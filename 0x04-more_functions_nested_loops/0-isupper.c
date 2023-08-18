5f75f459b726835043ef1586f9a4e9b2c6aebd32
assiaboudlal98 <boudlala22@gmail.com>
2023-08-18 13:54:21 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * check if c is upper otherwise it's 0 
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if its uppercase , otherwise its 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <== 90)
+		return (1);
+	else 
+		return(0);
+}
