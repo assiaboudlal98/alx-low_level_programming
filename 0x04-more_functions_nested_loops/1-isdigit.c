5d65a76d727277f4c2a2bf366189fe7444441b6c
assiaboudlal98 <boudlala22@gmail.com>
2023-08-18 14:34:18 -0700

    My first commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *_isupper - check if the input is digit  between 0-9
+ *_isdigit - check if the input is digit  between 0-9
  *
  * @c: input
  *
  * Return: 1 if it's a digit , 0 if it's not
  */
 
-int _isupper(int c)
+int _isdigit(int c);
 {
 	if (c >= 48 && c <= 57)
 		return (1);
