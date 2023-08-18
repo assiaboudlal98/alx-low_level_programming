029c8729f8ac1e7a37e5598fa3667c2a32097ad2
assiaboudlal98 <boudlala22@gmail.com>
2023-08-18 14:32:23 -0700

    My first commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -10,8 +10,8 @@
 
 int _isupper(int c)
 {
-        if (c >= 48 && c <= 57)
-                return (1);
-        else
-                return (0);
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
 }
