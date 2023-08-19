59d31bd678942b9a69bde85a53fc9ff241fc338c
assiaboudlal98 <boudlala22@gmail.com>
2023-08-18 18:28:52 -0700

    My first commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,7 +16,7 @@ void print_triangle(int size)
 		_putchar('\n');
 	else
 	{
-		for (hght = 1; ught <= size; hght++)
+		for (hght = 1; hght <= size; hght++)
 		{
 			for (base = 1; base <= size; base++)
 			{
