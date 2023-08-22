b5ff874f4df24c498347ad28233546eb3830bb2e
assiaboudlal98 <boudlala22@gmail.com>
2023-08-21 17:11:58 -0700

    My first commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -17,8 +17,8 @@ void rev_string(char *s)
 	for (l = 0; s[l] != '\0'; ++l)
 		;
 	/*swap the string by looping to half the string*/
-	for (i = 0; i< l / 2; i++)
-	{       
+	for (i = 0; i < l / 2; i++)
+	{
 		temp = s[i];
 		s[i] = s[l - 1 - 1]; /*-1 because the array starts from 0*/
 		s[l - 1 - i] = temp;
