94f7c1dca17e23d80cefcdd488cce6b923081d47
assiaboudlal98 <boudlala22@gmail.com>
2023-09-25 13:11:54 -0700

    a

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -11,15 +10,14 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * for alx project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -37,3 +35,4 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
+
