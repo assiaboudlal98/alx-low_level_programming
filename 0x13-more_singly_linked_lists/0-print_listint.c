35981f1c6df8393b2d8da328ca18f75f5625bd1d
assiaboudlal98 <boudlala22@gmail.com>
2023-09-25 13:04:57 -0700

    a

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,3 +1,5 @@
+#include "lists.h"
+
 /**
  * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
@@ -17,4 +19,3 @@ size_t print_listint(const listint_t *h)
 
 	return (j);
 }
-
